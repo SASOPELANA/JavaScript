d25badfe0b75a33c9d5068f8c7d01666d1c4d99d
Sergio Alejandro <sopekof@gmail.com>
2025-02-10 00:59:16 -0300

    Calculadora

---
+++ Calculadora/app.js
@@ -0,0 +1,17 @@
+// Agregar un valor a pantalla del boton de la calculadora
+
+function agregar(valor) {
+  document.getElementById("pantalla").value += valor;
+}
+
+// borrar el contenido de la pantalla
+function borrar() {
+  document.getElementById("pantalla").value = "";
+}
+
+// Calcular el resultado de la pantalla
+function calcular() {
+  const valorPantalla = document.getElementById("pantalla").value;
+  const resultado = eval(valorPantalla);
+  document.getElementById("pantalla").value = resultado;
+}
---
+++ String/app.js
@@ -0,0 +1,6 @@
+let string =
+  "             Apredender a usar JavaScript, para mejorar              ";
+
+let a = " y conseguir un mejor trabajo.";
+
+console.log(string.split("."));
